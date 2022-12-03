220effb3cc8388b6fd4772c8900899e1f58c6dbe
Irodaxon09 <tursunovairoda619@gmail.com>
2022-12-03 13:06:53 +0500

    animaton1

---
+++ b.js
@@ -0,0 +1,8 @@
+function ikki (){
+    let bir =document.getElementById("bir").value
+    let ikki =Number(bir )+1
+    document.getElementById("bir").value =ikki
+}
+function uch(){
+    document.getElementById("bir").value =0
+}
